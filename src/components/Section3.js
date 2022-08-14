94f41cb87735978d9c565b20623f221ea9a811db
Aran Diez <99818791+AranDiez@users.noreply.github.com>
2022-08-14 20:09:26 +0200

    changes in section 2

--- src/components/Section3.js
+++ src/components/Section3.js
@@ -3,8 +3,9 @@ import '../styles/components/Section3.scss';
 const Section3 = (props) => {
   return (
     <>
-      <h2 className="experience">EXPERIENCE</h2>
-      <h2 className="education">EDUCATION</h2>
+      <h2 className="experience">ABOUT ME</h2>
+      <h2 className="education">SKILLS</h2>
+      <h2 className="contact">CONTACT</h2>
     </>
   );
 };
